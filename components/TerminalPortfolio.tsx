c707cad621e554a7eb57e34db127ab7c914df5d1
neil-jay <neiljay@pcmender.online>
2024-10-23 19:05:19 +0530

    Removed Terminal component

--- components/TerminalPortfolio.tsx
+++ components/TerminalPortfolio.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useCallback } from 'react'
-import { Terminal } from 'lucide-react'
+// import { Terminal } from 'lucide-react'
 import Image from 'next/image'
 
 export default function TerminalPortfolio() {
